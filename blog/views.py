1eb40960b1dc752c5dd78fa7c2f5528fa9eb5418
Jonathan Heaney <jonathan.heaney@gmail.com>
2024-03-27 09:34:27 -0500

    Set up URLs/views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseNotFound, HttpResponseRedirect
+from django.urls import reverse
 
 # Create your views here.
+
+
+def index(request):
+    response_data = "Index"
+    return HttpResponse(response_data)
+
+
+def all_posts(request):
+    response_data = "All Posts"
+    return HttpResponse(response_data)
+
+
+def show_post(request, post):
+    response_data = f"Post Number {post}"
+    return HttpResponse(response_data)
